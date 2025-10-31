0b4480e2492aad165a84f61b4debbea673794b74
zwnewm <zwnewm@gmail.com>
2025-10-31 09:21:15 -0700

    Update Main.java
    first

--- Main.java
+++ Main.java
@@ -1,5 +1,29 @@
 public class Main {
    public static void main(String[] args) {
-      System.out.print("test");
+      
+      System.out.println(dateIsBefore(8,15,8,15));   
+      
+      
+   }
+   
+   public static boolean dateIsBefore(int m1, int d1, int m2, int d2) {
+   
+      if(m1 > m2){
+         return false;
+      }
+      
+      else if(m1 < m2){
+         return true;
+      }
+      
+      else {
+         if(d1 < d2){
+            return true;
+         }
+         else {
+            return false;
+         }
+      }
+   
    }
 }
